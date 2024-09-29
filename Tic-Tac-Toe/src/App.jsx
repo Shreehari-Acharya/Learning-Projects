9eda7dfadcece35bbb88457f9ecde325ba43a2fa
Shreehari Acharya <shreehari.acharya.06@gmail.com>
2024-09-29 12:27:26 +0530

    Made buttons for tic-tac-tow which will display X and O one by one

--- Tic-Tac-Toe/src/App.jsx
+++ Tic-Tac-Toe/src/App.jsx
@@ -1,7 +1,50 @@
+
 import { useState } from 'react'
+import './App.css'
 
-function App() {
 
+function Square({value, onSquareClick}) {
+  return(
+    <button className='square-btn' onClick={onSquareClick}>{value}</button>
+  )
 }
 
-export default App
+function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+  function handleClick(i){
+    const nextSquares = squares.slice();
+    if(xIsNext){
+      nextSquares[i] = "X";
+    }
+    else{
+      nextSquares[i] = "O"
+    }
+    setXIsNext(!xIsNext)
+    setSquares(nextSquares);
+  }
+  return(
+    <div className="board">
+       <div className='board-row'>
+        {squares.slice(0,3).map((square,index) => <Square key={index} value={square} onSquareClick={()=> handleClick(index)}/>)}
+      </div>
+      <div className='board-row'>
+        {squares.slice(3,6).map((sqaure,index) => <Square key={index+3} value={sqaure} onSquareClick={()=> handleClick(index+3)}/>)}
+      </div>
+      <div className='board-row'>
+        {squares.slice(6,9).map((square,index) => <Square key={index+6} value={square} onSquareClick={()=> handleClick(index+6)}/>)}
+      </div>
+    </div>
+  )
+}
+
+
+function Game() {
+  return(
+      <div className="game">
+          <Board/>
+      </div>
+  )
+}
+
+export default Game
